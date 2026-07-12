5a6e2b099edb90a8b81f1d4c51207d0ad009c56f
vinitshirbhate <vinit.shirbhate23@vit.edu>
2026-07-12 22:44:34 +0530

    refactor: switched wisper-small for asr

--- asr.py
+++ asr.py
@@ -1,13 +1,53 @@
-from transformers import pipeline
+import torch
+import librosa
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
 
-# Load the ASR pipeline
-pipe = pipeline(
-    task="automatic-speech-recognition",
-    model="parthiv11/indic_whisper_nodcil",
-    device="cuda:0"  
+import transformers
+
+
+# Device
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using:", DEVICE)
+
+# Load multilingual Whisper model
+MODEL_NAME = "openai/whisper-small"
+
+processor = WhisperProcessor.from_pretrained(MODEL_NAME)
+model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME).to(DEVICE)
+
+# Load audio (Whisper expects 16 kHz mono)
+audio, sr = librosa.load("gemma_tts\samples\hindi.wav", sr=16000, mono=True)
+
+print(f"Sample rate: {sr}")
+print(f"Duration: {len(audio)/sr:.2f} seconds")
+
+# Create input features
+inputs = processor(
+    audio,
+    sampling_rate=16000,
+    return_tensors="pt"
 )
 
-# Transcribe an audio file
-result = pipe("audio.wav")
+input_features = inputs.input_features.to(DEVICE)
 
-print(result["text"])
+# Generate Marathi transcription
+predicted_ids = model.generate(
+    input_features,
+    language="mr",
+    task="transcribe"
+)
+
+# Decode
+transcription = processor.batch_decode(
+    predicted_ids,
+    skip_special_tokens=True
+)[0]
+
+print("\nTranscription:")
+print(transcription)
+
+
+print("Transformers:", transformers.__version__)
+print("Torch:", torch.__version__)
+print("Duration:", len(audio)/sr)
+print("Model:", MODEL_NAME)
